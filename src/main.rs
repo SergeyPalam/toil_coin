0ad31fbdfed6a8bd5c60437f7349555c1cd1bf62
Sergey Palamarchuk <spalamarchuk787@gmail.com>
2024-02-05 17:43:14 +0300

    Implement init logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use flexi_logger::{FileSpec, LogSpecification, Logger};
+
+fn init_logger(){
+    if cfg!(debug_assertions){
+        Logger::with(LogSpecification::trace())
+        .log_to_file(FileSpec::default())
+        .start().unwrap();
+    }else{
+        Logger::with(LogSpecification::info())
+        .log_to_file(FileSpec::default())
+        .start().unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
-    //some terrable
+    init_logger();
 }
